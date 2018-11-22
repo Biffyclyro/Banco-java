f497846a1e40a1ba679d54d3a60d77ef7d5c410f
Biffyclyro <joao-senna1@hotmail.com>
2018-11-22 15:54:13 -0200

    Add files via upload

---
+++ Banco/src/model/Funcionario.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private double salario;
+    
+    
+    public Funcionario(String nome, String cpf, double salario){
+        this.nome=nome;
+        this.cpf=nome;
+        this.salario=salario;
+    }
+    
+    
+    
+    
+    
+}
---
+++ Banco/src/model/Gerente.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Gerente extends Funcionario {
+    private int senha;
+    private int numeroDeFuncionariosGerenciados;
+
+    public Gerente(int senha, int numeroDeFuncionariosGerenciados, String nome, String cpf, double salario) {
+        super(nome, cpf, salario);
+        this.senha = senha;
+        this.numeroDeFuncionariosGerenciados = numeroDeFuncionariosGerenciados;
+    }
+    
+    public void setSenha(int senha){
+        this.senha=senha;
+        
+    }
+    
+    public int getSenha(){
+        return senha;
+    }
+
+    public int getNumeroDeFuncionariosGerenciados() {
+        return numeroDeFuncionariosGerenciados;
+    }
+
+    public void setNumeroDeFuncionariosGerenciados(int numeroDeFuncionariosGerenciados) {
+        this.numeroDeFuncionariosGerenciados = numeroDeFuncionariosGerenciados;
+    }
+    
+    
+    
+    public boolean autentica(int senha){
+        if(this.senha==senha){
+            System.out.println("Acesso permitido");
+            return true;
+        }else{
+            System.out.println("Acesso negado");
+            return false;
+        }
+    }
+    
+    
+    
+    
+    
+    
+
+}
